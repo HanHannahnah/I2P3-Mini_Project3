083586a611bc16c3b6e3713f9440afce68842597
HanHannahnah <hancc930109@gmail.com>
2023-06-20 12:06:26 +0800

    add submission files

---
+++ src/player/submission.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <fstream>
+#include <limits.h>
+
+#include "../config.hpp"
+#include "../state/state.hpp"
+#include "../policy/Alphabeta.hpp"
+
+
+State* root;
+
+/**
+ * @brief Read the board from the file
+ * 
+ * @param fin 
+ */
+void read_board(std::ifstream& fin) {
+  Board board;
+  int player;
+  fin >> player;
+
+  for (int pl=0; pl<2; pl++) {
+    for (int i=0; i<BOARD_H; i++) {
+      for (int j=0; j<BOARD_W; j++) {
+        int c; fin >> c;
+        // std::cout << c << " ";
+        board.board[pl][i][j] = c;
+      }
+      // std::cout << std::endl;
+    }
+  }
+  root = new State(board, player);
+  root->get_legal_actions();
+}
+
+
+/**
+ * @brief choose a move and then write it into output file
+ * 
+ * @param fout 
+ */
+void write_valid_spot(std::ofstream& fout) {
+  // Keep updating the output until getting killed.
+  Alphabeta a;
+  a.highest_value = INT_MIN;
+
+  if(!root->legal_actions.size())
+      root->get_legal_actions();
+
+  int depth = 4;
+
+  //while(true) {
+    // Choose a random spot.
+
+    for (auto m : root->legal_actions){
+      //auto move = root->legal_actions[(rand()+depth)%(root->legal_actions).size()];
+      auto move = Alphabeta::get_move(root, m, depth, a);
+      fout << move.first.first << " " << move.first.second << " "\
+          << move.second.first << " " << move.second.second << std::endl;
+    
+      fout.flush();
+    }
+
+    //depth++;
+  //}
+
+}
+
+
+/**
+ * @brief Main function for player
+ * 
+ * @param argv 
+ * @return int 
+ */
+int main(int, char** argv) {
+  srand(RANDOM_SEED);
+  std::ifstream fin(argv[1]);
+  std::ofstream fout(argv[2]);
+
+  read_board(fin);
+  write_valid_spot(fout);
+
+  fin.close();
+  fout.close();
+  return 0;
+}
---
+++ src/policy/submission.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include "../state/state.hpp"
+
+
+/**
+ * @brief Policy class for random policy, 
+ * your policy class should have get_move method
+ */
+class Alphabeta{
+public:
+  Move best_move;
+  int highest_value;
+  int vis[];
+  static Move get_move(State *state, Move m, int depth, Alphabeta a);
+  int alphabeta(State& state, int depth, int alpha, int beta, bool maximizingPlayer);
+};
